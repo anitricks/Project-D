48d4d3bf43201eb9764717b667b7d6ec5055d448
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-17 11:25:42 -0400

    New Movement
    - tried out new movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,11 @@ public class PlayerMovement : MonoBehaviour
 	Animator anim;
 	public Boundary bounds;
 
+    bool hasSpinned; // can be used to start game as well
+    bool wasSpinningLeft;
+    int rotateSpeed = 5;
+    float rotateBufferSpeed = 0.1f;
+
 	void Start ()
 	{
 		body = GetComponent<Rigidbody2D> ();
@@ -35,7 +40,9 @@ public class PlayerMovement : MonoBehaviour
 
 		body.position = new Vector2 (Mathf.Clamp (body.position.x, bounds.xMin, bounds.xMax), body.position.y);
 
-		anim.SetFloat ("Speed", move.x);
+        //anim.SetFloat ("Speed", move.x);
+
+        Rotate(x_move);
 
 		//Debug.Log (Mathf.Clamp (body.velocity.x, -1, 1) * 0.8f);
 	}
@@ -48,4 +55,58 @@ public class PlayerMovement : MonoBehaviour
 		}
 	}
 
+    // ** Attention:
+    // z degree is reversed in unity
+    // z > 0, turn left
+
+    void Rotate(float vel)
+    {
+        float deg = 0;
+
+        if (vel == 0)
+        {
+            float zRotation = transform.rotation.eulerAngles.z;
+
+            //if(!hasSpinned || zRotation % 360 ==0)
+            //   return;
+
+
+            if (!hasSpinned || (int)zRotation == 0 || (int)zRotation == 360)
+                return;
+
+            float buffer=0;
+            float targetDeg = 0;
+
+            if (wasSpinningLeft)
+            {
+                targetDeg = 360; // ** reversed. normal sense would be 0
+                buffer = Mathf.Lerp(zRotation, targetDeg, rotateBufferSpeed);
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, buffer));
+            }
+
+            else{
+                targetDeg = 0; // ** reversed. normal sense would be 360
+
+                buffer = Mathf.Lerp(zRotation, targetDeg, rotateBufferSpeed);
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, buffer));
+            }
+        }
+            
+        else if (vel > 0){
+            deg = -1 * rotateSpeed;
+            if(!hasSpinned)
+                hasSpinned = true;
+            wasSpinningLeft = false;
+        }
+        else if (vel < 0){
+            deg = 1 * rotateSpeed; 
+            if(!hasSpinned)
+                hasSpinned = true;
+             wasSpinningLeft = true;
+        }
+
+        transform.Rotate(Vector3.forward,deg);
+    }
 }
