747df8ae8c9424670e0ff10886586c1db745860d
Trishant Bhatija <anitricks@live.com>
2015-04-01 20:50:30 -0400

    Updated menu

---
+++ Assets/Scripts/MenuLinks.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLinks : MonoBehaviour
+{
+	bool already = false;
+	Animator anim;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	public void showHelp ()
+	{
+		if (!already) {
+			anim.SetTrigger ("Help");
+			already = true;
+		} else {
+			anim.SetTrigger ("Help-Rev");
+			already = false;
+		}
+	}
+}
